a60c44cd66bd3b6acb9adf0b5b03fee0a497b16c
theborg <theborg@grautier.com>
2011-05-15 18:33:27 +0000

    - ADD new Tool (Test)

---
+++ tools/label.php
@@ -0,0 +1,27 @@
+<html>
+ <body class="body">
+  <head>
+   <link rel="StyleSheet" href="../css/partdb.css" type="text/css" />
+
+<table class="table">
+	<tr>
+		<td class="tdtop">
+		Labels
+		</td>
+	</tr>
+	<tr>
+		<td class="tdtext">
+		<b>SMD-Wiederstände</b>
+		<b>SMD-Kondensatoren</b>
+		<b>SMD-Dioden</b>
+		<b>SMD-Spulen</b>
+		<b>SMD-Transistoren</b>
+		<b>SMD-LEDs</b>
+		<b>SMD-ICs</b>
+		</td>
+	</tr>
+</table>
+
+  </head>
+ </body>
+</html>
