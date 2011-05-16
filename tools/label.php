9abeebf13201f3792eb33bbdb5ceb6a3f4b025dd
theborg0815 <theborg0815@gmail.com>
2011-05-16 21:10:30 +0000

    - EDIT label.php

--- tools/label.php
+++ tools/label.php
@@ -12,24 +12,35 @@
 	<tr>
 		<td class="tdtext">
 		<b>SMD-Wiederstände</b>
-		3-Stellig
-		<img src="label/100.png"></img>
-		<img src="label/101.png"></img>
-		<img src="label/102.png"></img>			
-		4-Stellig
-		<img src="label/1001.png"></img>
-		<img src="label/1002.png"></img>
-		<img src="label/1003.png"></img>	
-		4-Stellig mit R		
-		<img src="label/R10.png"></img>
-		<img src="label/10R2.png"></img>
-		<img src="label/100R.png"></img>	
+			3-Stellig </br>
+			Die ersten beiden Stellen sind der Wert, die letzte Stelle ist der Exponent z.b. </br>
+				<img src="label/100.png" class=\"labelbild\"></img> 10 Ohm </br> 
+				<img src="label/101.png" class=\"labelbild\"></img> 100 Ohm </br>
+				<img src="label/102.png" class=\"labelbild\"></img> 1000 Ohm </br>
+			</br>
+			4-Stellig </br>
+			Die ersten drei Stellen sind der Wert, die letzte Stelle ist der Exponent z.b. </br>
+				<img src="label/1001.png" class=\"labelbild\"></img> 1000 Ohm </br>
+				<img src="label/1002.png" class=\"labelbild\"></img> 10000 Ohm </br>
+				<img src="label/1003.png" class=\"labelbild\"></img> 100000 Ohm </br>
+			</br>
+			Mehrstellig mit R </br>		
+			Das R zählt als Dezimalpunkt z.b. </br>
+				<img src="label/R10.png" class=\"labelbild\"></img> 0,10 Ohm </br>
+				<img src="label/10R2.png" class=\"labelbild\"></img> 10,2 Ohm </br>
+				<img src="label/100R.png" class=\"labelbild\"></img>	100 Ohm </br>
+			</br>
+			</br>
 		<b>SMD-Kondensatoren</b>
-
+			Tantal 
+			Elkos
+			Keramik
+			</br>
+			</br>
 		<b>SMD-Dioden</b>
-
+		   
 		<b>SMD-Spulen</b>
-
+		
 		<b>SMD-Transistoren</b>
 
 		<b>SMD-LEDs</b>
