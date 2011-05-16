ee4156a910811497076cae1d0215c9d8f9f6873e
theborg0815 <theborg0815@gmail.com>
2011-05-16 07:25:27 +0000

    - ADD Label Bilder (Rs)

--- tools/label.php
+++ tools/label.php
@@ -12,11 +12,28 @@
 	<tr>
 		<td class="tdtext">
 		<b>SMD-Wiederstände</b>
+		3-Stellig
+		<img src="label/100.png"></img>
+		<img src="label/101.png"></img>
+		<img src="label/102.png"></img>			
+		4-Stellig
+		<img src="label/1001.png"></img>
+		<img src="label/1002.png"></img>
+		<img src="label/1003.png"></img>	
+		4-Stellig mit R		
+		<img src="label/R10.png"></img>
+		<img src="label/10R2.png"></img>
+		<img src="label/100R.png"></img>	
 		<b>SMD-Kondensatoren</b>
+
 		<b>SMD-Dioden</b>
+
 		<b>SMD-Spulen</b>
+
 		<b>SMD-Transistoren</b>
+
 		<b>SMD-LEDs</b>
+
 		<b>SMD-ICs</b>
 		</td>
 	</tr>
