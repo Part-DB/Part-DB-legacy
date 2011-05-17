8c834674ae46075c6dadfb2a3475a48ed267836a
theborg0815 <theborg0815@gmail.com>
2011-05-17 21:21:11 +0000

    - EDIT label.php

--- tools/label.php
+++ tools/label.php
@@ -6,49 +6,78 @@
 <table class="table">
 	<tr>
 		<td class="tdtop">
-		Labels
+		SMD-Wiederst&aumlnde
 		</td>
 	</tr>
 	<tr>
 		<td class="tdtext">
-		<b>SMD-Wiederstände</b>
-			3-Stellig </br>
-			Die ersten beiden Stellen sind der Wert, die letzte Stelle ist der Exponent z.b. </br>
-				<img src="label/100.png" class=\"labelbild\"></img> 10 Ohm </br> 
-				<img src="label/101.png" class=\"labelbild\"></img> 100 Ohm </br>
-				<img src="label/102.png" class=\"labelbild\"></img> 1000 Ohm </br>
-			</br>
-			4-Stellig </br>
-			Die ersten drei Stellen sind der Wert, die letzte Stelle ist der Exponent z.b. </br>
-				<img src="label/1001.png" class=\"labelbild\"></img> 1000 Ohm </br>
-				<img src="label/1002.png" class=\"labelbild\"></img> 10000 Ohm </br>
-				<img src="label/1003.png" class=\"labelbild\"></img> 100000 Ohm </br>
-			</br>
-			Mehrstellig mit R </br>		
-			Das R zählt als Dezimalpunkt z.b. </br>
-				<img src="label/R10.png" class=\"labelbild\"></img> 0,10 Ohm </br>
-				<img src="label/10R2.png" class=\"labelbild\"></img> 10,2 Ohm </br>
-				<img src="label/100R.png" class=\"labelbild\"></img>	100 Ohm </br>
-			</br>
-			</br>
-		<b>SMD-Kondensatoren</b>
-			Tantal 
-			Elkos
-			Keramik
-			</br>
-			</br>
-		<b>SMD-Dioden</b>
-		   
-		<b>SMD-Spulen</b>
-		
-		<b>SMD-Transistoren</b>
-
-		<b>SMD-LEDs</b>
-
-		<b>SMD-ICs</b>
+			<table>			
+			<tr>
+			   <td><b>3-Stellig</b></br>Die ersten beiden Stellen sind der Wert,</br> die letzte Stelle ist der Exponent z.b.</td>
+				<td><img src="label/100.png" class=\"labelbild\"></img> 10 Ohm</td>
+				<td><img src="label/101.png" class=\"labelbild\"></img> 100 Ohm</td>
+				<td><img src="label/102.png" class=\"labelbild\"></img> 1000 Ohm</td>
+			</tr>
+			<tr>
+				<td><b>4-Stellig</b></br>Die ersten drei Stellen sind der Wert,</br> die letzte Stelle ist der Exponent z.b.</td>
+				<td><img src="label/1001.png" class=\"labelbild\"></img> 1000 Ohm</td>
+				<td><img src="label/1002.png" class=\"labelbild\"></img> 10000 Ohm</td>
+				<td><img src="label/1003.png" class=\"labelbild\"></img> 100000 Ohm</td>
+			</tr>
+			<tr>
+				<td><b>Mehrstellig mit R</b></br>Das R z&aumlhlt als Dezimalpunkt z.b.</td>
+				<td><img src="label/R10.png" class=\"labelbild\"></img> 0,10 Ohm</td>
+				<td><img src="label/10R2.png" class=\"labelbild\"></img> 10,2 Ohm</td>
+				<td><img src="label/100R.png" class=\"labelbild\"></img>	100 Ohm</td>
+			</tr>
+			</table>
 		</td>
 	</tr>
 </table>
+
+<table class="tablenone">
+</br>
+</table>
+
+<table class="table">
+	<tr>
+		<td class="tdtop">
+		SMD-Kondensatoren
+		</td>
+	</tr>
+	<tr>
+		<td class="tdtext">
+			<table>			
+			<tr>
+				<td><b>Tantal</b></br>Die ersten beiden Stellen sind der Wert in pF,</br>die letzte der Exponent.</br>Die untere Zahl ist die Spannungsfestigkeit in Volt</td>
+				<td><img src="label/246-20.png" class=\"labelbild\"></img>24µF/20V</td> 
+			</tr>
+			<tr>
+				<td><b>Elkos</b></br>Die Zahlen geben den wert in µF an,</br>der Buchstabe ist das Trennzeichen,</br> und gibt gleichzeitig die Spannungsfestigkeit an</br>C=6,3V D=10V E=16V F=25V G=40V H=63V</td>
+				<td><img src="label/3F3.png" class=\"labelbild\"></img>3,3µF/25V</td> 
+			</tr>
+			</table>
+		</td>
+	</tr>
+</table>	
+		
+			ALU-Elko</br>
+			</br>
+			</br>
+		<b>SMD-Dioden</b></br>
+		   </br>
+		   </br>
+		<b>SMD-Spulen</b></br>
+				<img src="label/221K.png" class=\"labelbild\"></img> 10 Ohm </br> 		
+		<b>SMD-Transistoren</b></br>
+		</br>
+		</br>
+		<b>SMD-LEDs</b></br>
+		</br>
+		</br>
+
+		<b>SMD-ICs</b>
+
 
   </head>
  </body>
