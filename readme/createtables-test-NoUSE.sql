9b0fbdfb06fc4e48546b31fbee3c67054990eed1
theborg0815 <theborg0815@gmail.com>
2012-01-24 18:50:58 +0000

    DB FIX - for new DATABASES and NEW MYSQL Versions.

--- readme/createtables-test-NoUSE.sql
+++ readme/createtables-test-NoUSE.sql
@@ -140,11 +140,12 @@ CREATE TABLE `suppliers` (
 -- 
 -- Table structure for table `devices`
 -- 
+
 CREATE TABLE `devices` (
   `id` int(11) NOT NULL auto_increment,
   `name` mediumtext NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM;
 
 -- --------------------------------------------------------
 
@@ -156,4 +157,5 @@ CREATE TABLE `part_device` (
   `id_part` int(11) NOT NULL default '0',
   `id_device` int(11) NOT NULL default '0',
   `quantity` int(11) NOT NULL default '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY  (`id_part`)
+) ENGINE=MyISAM;
