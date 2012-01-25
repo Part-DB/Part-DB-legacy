27d04a47f6b42c5469b0109deefe0454e53600eb
theborg0815 <theborg0815@gmail.com>
2012-01-25 11:51:28 +0000

    Rename DB files, Update to V0.2.0, Complett DB for New Install, Older Versions

--- readme/createtables-OLD-DB.sql
+++ readme/createtables-OLD-DB.sql
@@ -148,26 +148,3 @@ CREATE TABLE `suppliers` (
   `name` tinytext NOT NULL,
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM;
-
--- --------------------------------------------------------
-
--- 
--- Table structure for table `devices`
--- 
-CREATE TABLE `devices` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` mediumtext NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
--- 
--- Tabellenstruktur für Tabelle `part_device`
--- 
-
-CREATE TABLE `part_device` (
-  `id_part` int(11) NOT NULL default '0',
-  `id_device` int(11) NOT NULL default '0',
-  `quantity` int(11) NOT NULL default '0'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
