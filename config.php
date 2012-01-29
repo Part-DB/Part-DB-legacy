47e1446e84bc07769ac29177e7e502ce07b61e9c
lipschinski_dennis <lipschinski_dennis@onlinehome.de>
2012-01-29 18:01:05 +0000

    - changed to default config table

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
         
     /* Enter your MySQL username and password in config.php */
     $mysql_server  = "localhost";
-    $db_user       = "test-part-db";
-    $db_password   = "test-part-db";
+    $db_user       = "part-db";
+    $db_password   = "PARTdb";
 
     /* this is the name of the mysql database */
-    $database      = "test-part-db";
+    $database      = "part-db";
 
     /* choose your currency */
     $currency      = "&euro;";
