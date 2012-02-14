48df25c3b6a11c3b7e1d935da8654291a5417f18
bubbles.red <bubbles.red@gmail.com>
2012-02-14 05:48:36 +0000

    fix: typo

--- help.php
+++ help.php
@@ -22,14 +22,14 @@
         ICs und einige Standard-Bauteile sind schon vorhanden die Bezeichnung daf&uuml;r findet man anhand der Bildnamen in "Tools->Footprints" raus.<br>
         <br>
         <b>Sicherheitseinstellungen (.htaccess/.htpasswd)</b><br>
-        Um die Datenbank vor unerlaubten Zugriff zu sch&uuml;tzen sollte man unbedingt das Verzeichnis mit einem Passwort sch&uuml;tzen, am sinnvollsten ist es, dies serverseitig mit .htacsess zu machen. Eine Anleitung daf&uuml;r findet man unter folgendem <a href="http://www.grammiweb.de/anleitungen/ka_htaccess.shtml">Link</a>.<br>
+        Um die Datenbank vor unerlaubten Zugriff zu sch&uuml;tzen sollte man unbedingt das Verzeichnis mit einem Passwort sch&uuml;tzen, am sinnvollsten ist es, dies serverseitig mit .htaccess zu machen. Eine Anleitung daf&uuml;r findet man unter folgendem <a href="http://www.grammiweb.de/anleitungen/ka_htaccess.shtml">Link</a>.<br>
         <br>
         <b>Template &auml;ndern</b><br>
         Momentan sind zwei Templates f&uuml;r die Part-DB vorhanden. Zum Wechseln einfach die gew&uuml;nschte Dateie in ../css/partdb.css umbenennen.<br>
         <br>
         <b>&Ouml;ffentliche Liste</b><br>
         Am einfachsten geht es, wenn ihr auf dem Server einen extra Ordner erstellt. In diesen kopiert ihr die openlist.php, stats.php, config.php, lib.php und den CSS Ordner.
-                Alternativ k&ouml;nnt Ihr auch den Ordner ../partdb/openlist/ mit .htacsess wieder freigeben dieses klappt aber nur wenn der Webserver dieses erlaubt. 
+                Alternativ k&ouml;nnt Ihr auch den Ordner ../partdb/openlist/ mit .htaccess wieder freigeben dieses klappt aber nur wenn der Webserver dieses erlaubt. 
         </td>
     </tr>
 </table>
