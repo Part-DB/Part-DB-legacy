b3e4bde20bd3e010d33b70840fb2664874c69387
bubbles.red <bubbles.red@gmail.com>
2012-04-02 21:41:29 +0000

    change: try to fix array_merge_error

--- lib.php
+++ lib.php
@@ -651,7 +651,7 @@
         // Does not support flag GLOB_BRACE
         function glob_recursive($pattern, $flags = 0)
         {
-            $files = glob($pattern, $flags);
+            $files = array();
             foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
             {
                 $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
@@ -663,8 +663,6 @@
 
     function footprint_picture_exists( $footprint)
     {
-        //$link = "tools/footprints/". $footprint .".png";
-        //return( is_file( $link) ? $link : false);
         $res = glob_recursive( "tools/footprints/". $footprint .".png");
         return( $res ? $res[0] : false);
     }
