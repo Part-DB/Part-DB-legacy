825775c60d1fefe6500104cddd201eb9bf1474c2
Jan Böhmer <jan.h.boehmer@gmx.de>
2017-01-01 17:51:15 +0100

    Dont test against PHP 5.4, this wont work.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,9 @@
 //set_include_path(get_include_path() . PATH_SEPARATOR . '../../lib/lib.php');
 define( 'BASE', "." );
 include_once "lib/autoloader.php";
+
+//Fake function global debug funtion
+function debug($string)
+{
+    //Do nothing here
+}
