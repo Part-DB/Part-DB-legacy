53ff616491693f6633ac737f50c8d7b585cfb5d9
Jan Böhmer <jan.h.boehmer@gmx.de>
2017-01-01 22:58:08 +0100

    Added some more SystemVersion Tests.

--- tests/lib/SystemVersionTest.php
+++ tests/lib/SystemVersionTest.php
@@ -19,7 +19,7 @@ class SystemVersionTest extends PHPUnit_Framework_TestCase
     public function test_stable()
     {
         $version = new SystemVersion("0.3.4");
-        $this->assertEquals($version->get_version_type(),"stable");
+        $this->assertEquals("stable",$version->get_version_type());
     }
 
     /*
@@ -28,7 +28,7 @@ class SystemVersionTest extends PHPUnit_Framework_TestCase
     public function test_unstable()
     {
         $version = new SystemVersion("0.3.4.RC1");
-        $this->assertEquals($version->get_version_type(),"unstable");
+        $this->assertEquals("unstable",$version->get_version_type());
     }
 
     /**
@@ -82,6 +82,41 @@ class SystemVersionTest extends PHPUnit_Framework_TestCase
         $ver1 = new SystemVersion("0.2.1.RC1");
         $ver2 = new SystemVersion("0.2.1");
         $this->assertTrue($ver2->is_newer_than($ver1));
+    }
+
+    public function test_as_string_internal()
+    {
+        $ver = new SystemVersion("0.3.4.RC1");
+        $this->assertEquals("0.3.4.RC1",$ver->as_string(true));
+    }
+
+    public function test_as_string()
+    {
+        $ver = new SystemVersion("0.3.4.RC1");
+        $this->assertEquals("0.3.4 RC1", $ver->as_string(false));
+    }
+
+    public function test_as_string_hide_rc()
+    {
+        $ver = new SystemVersion("0.3.4.RC1");
+        $this->assertEquals("0.3.4",$ver->as_string(false,true));
+    }
+
+    public function test_as_string_show_type()
+    {
+        $ver = new SystemVersion("0.3.4.RC1");
+        $this->assertEquals("0.3.4 RC1 (unstable)",$ver->as_string(false,false,false,true));
+    }
+
+    /**
+     * Tests if SystemVersion::get_installed_version() returns a SystemVersion obj.
+     */
+    public function test_get_installed_version()
+    {
+        global $config;
+        $config['system']['version'] = "0.3.4.RC1";
+        $version = SystemVersion::get_installed_version();
+        $this->assertInstanceOf(SystemVersion::class, $version);
     }
 
 }
