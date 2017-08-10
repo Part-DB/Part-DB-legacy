d0a69e8e4e0d1a4251d2f5dc5b490d1134cb5a25
Jan Böhmer <Jan.h.boehmer@gmx.de>
2017-08-10 18:57:43 +0200

    Pooled all DB request for Part creation in PartContainingDBElement::get:parts().

--- lib/class.PartsContainingDBElement.php
+++ lib/class.PartsContainingDBElement.php
@@ -211,7 +211,7 @@
                     $query = $query . " OR ".$parts_rowname."= ".$element->get_id();
                 }
                 */
-                $query = 'SELECT id FROM parts WHERE '.$parts_rowname.'=?';
+                $query = 'SELECT parts.* FROM parts WHERE '.$parts_rowname.'=?';
                 $vals = array($this->get_id());
 
                 foreach($subelements as $element)
@@ -228,7 +228,7 @@
                 $query_data = $this->database->query($query, $vals);
 
                 foreach ($query_data as $row)
-                    $this->parts[] = new Part($this->database, $this->current_user, $this->log, $row['id']);
+                    $this->parts[] = new Part($this->database, $this->current_user, $this->log, $row['id'], $row);
 
                 usort($this->parts, 'PartsContainingDBElement::usort_compare');
             }
