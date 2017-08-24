447dcb368d7a87af8e58ae7d35069c4d0cb55055
Jan Böhmer <Jan.h.boehmer@gmx.de>
2017-08-24 13:43:11 +0200

    Use composer for Smarty and BBCodeParser.

--- start_session.php
+++ start_session.php
@@ -284,33 +284,6 @@
     textdomain($domain);
 
 
-
-    /********************************************************************************
-    *
-    *   autoload function for classes
-    *
-    *********************************************************************************/
-    // __autoload should not be used,  because only one handler can be used! Smarty doesnt work if this is overwritten
-    /*function load($classname)
-    {
-        $filename = "";
-        if (strpos($classname, 'vlib') === 0)
-        {
-            $filename = BASE.'/lib/vlib/'.$classname.'.php';
-        }
-        //Smarty has its own loader, so ignore smarty classes or this will produce many warnings!
-        else if(strpos(strtolower($classname), 'smarty') == false ) {
-            $filename = BASE . '/lib/class.' . $classname . '.php';
-        }
-
-        //Only load file if it really exists
-        if(is_file($filename))
-        {
-            include_once($filename);
-        }
-    }*/
-
-
     /********************************************************************************
     *
     *   include libraries
@@ -328,11 +301,5 @@
      ********************************************************************************/
 
     include_once (BASE.'/lib/autoloader.php');
-
-    include_once(BASE.'/lib/smarty/Autoloader.php');
-    Smarty_Autoloader::register();
-
-    //Load BBCode Parser
-    include_once(BASE.'/lib/bbcode/Autoloader.php');
-
-    //spl_autoload_register("load");
+    //Include Composer autoloader
+    require 'vendor/autoload.php';
