dae51b6f271d083656759dcc477bb33ddc99c6e8
Jan Böhmer <Jan.h.boehmer@gmx.de>
2017-08-29 16:13:29 +0200

    Fixed bug from last commit.

--- updates/db_update_steps.php
+++ updates/db_update_steps.php
@@ -23,7 +23,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 */
 
-include_once(BASE.'/updates/db_migration_functions');
+include_once(BASE.'/updates/db_migration_functions.php');
 
 /*
  * DATABASE UPDATE STEPS:
