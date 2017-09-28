24e5ac830323b74be90e180e726a5daa78f4f181
Jan Böhmer <Jan.h.boehmer@gmx.de>
2017-09-28 16:29:43 +0200

    Copied rmove to UpdateWorker, so it can work even when inc/lib.php was removed.

--- lib/Updater/UpdateWorker.php
+++ lib/Updater/UpdateWorker.php
@@ -156,12 +156,12 @@ class UpdateWorker
                 $movedir = false;
                 foreach ($files as $file) {
                     if (strpos($file, "Part-DB-") !== false) {
-                        $movedir = $fle;
+                        $movedir = $file;
                     }
                 }
                 //When needed move files and clean up
                 if ($movedir !== false) {
-                    rmove($dirname . "/" . $movedir, $dirname);
+                    static::rmove($dirname . "/" . $movedir, $dirname);
                     rmdir($dirname . "/" . $movedir);
                 }
 
@@ -175,5 +175,38 @@ class UpdateWorker
                 return false;
             }
         }
+    }
+
+    /**
+     * Recursively move files from one directory to another
+     *
+     * @param String $src - Source of files being moved
+     * @param String $dest - Destination of files being moved
+     */
+    protected function rmove($src, $dest)
+    {
+
+        // If source is not a directory stop processing
+        if (!is_dir($src)) return false;
+
+        // If the destination directory does not exist create it
+        if (!is_dir($dest)) {
+            if (!mkdir($dest)) {
+                // If the destination directory could not be created stop processing
+                return false;
+            }
+        }
+
+        // Open the source directory to read in files
+        $i = new \DirectoryIterator($src);
+        foreach($i as $f) {
+            if ($f->isFile()) {
+                rename($f->getRealPath(), "$dest/" . $f->getFilename());
+            } elseif (!$f->isDot() && $f->isDir()) {
+                rmove($f->getRealPath(), "$dest/$f");
+                rmdir($f->getRealPath());
+            }
+        }
+        unlink($src);
     }
 }
