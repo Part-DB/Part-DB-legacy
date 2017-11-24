68ee1f3673048b509301ec365e963a818c894a38
Jan Böhmer <Jan.h.boehmer@gmx.de>
2017-11-24 18:11:26 +0100

    Show an explaination for 0 value in session cookie lifetime.

--- system_config.php
+++ system_config.php
@@ -354,7 +354,7 @@ $html->setVariable('is_online_demo', $config['is_online_demo'], 'boolean');
 $html->setVariable('using_https', isUsingHTTPS(), 'boolean');
 $html->setVariable('max_input_vars', ini_get('max_input_vars'), 'string');
 $html->setVariable('max_upload_filesize', ini_get('upload_max_filesize'), 'string');
-$html->setVariable('session_cookie_lifetime', ini_get('session.cookie_lifetime'), 'string');
+$html->setVariable('session_cookie_lifetime', ini_get('session.cookie_lifetime') > 0 ? ini_get('session.cookie_lifetime') . "s" : _("Bis zum Schließen des Browsers"), 'string');
 $html->setVariable('session_gc_maxlifetime', ini_get('session.gc_maxlifetime'), 'string');
 
 //Part properties
